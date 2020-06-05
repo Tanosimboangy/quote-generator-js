53e44e6bddc16e3c4e0449be002ca0ceb53bc4c1
Tanosimboangy <jacquit.aja@onja.org>
2020-06-05 12:02:31 +0300

    checking the variable

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,14 +1,15 @@
- var firstQuote = [`Vero` , `Paul`, `Frank`, `Flore`];
- var secondQuote = [`loathe`, `hates`, `loves`, `ignores`, `abhor`];
- var thirdQuote = [`bein alone`, `the book`, `the new phone.`, `the school system`];
+var quoteNumber = prompt("How many quotes you would want?");
 
- function generateQuote() {
-   var randomNumber1 = Math.floor(Math.random() * (firstQuote.length));
-   var randomNumber2 = Math.floor(Math.random() * (secondQuote.length));
-   var randomNumber4 = Math.floor(Math.random() * (thirdQuote.length));
-   for (let i = 0; i < firstQuote.length + secondQuote.length + thirdQuote.length; i++)
-     var randomQuote = firstQuote[randomNumber1] + secondQuote[randomNumber2] + thirdQuote[randomNumber4];
- }
+  var firstQuote = ["Vero" , "Paul", "Frank", "Flore"];
+  var secondQuote = ["loathe", "hates", "loves", "ignores", "abhor"];
+  var thirdQuote = ["bein alone", "the book", "the new phone.", "your method"];
 
-console.log(Math.floor(Math.random() * (beg.length)));
-var quoteNumber = prompt(`How many quotes you would like to generate?`);
+  function generateQuote() {
+    var randomNumber1 = Math.floor(Math.random() * (firstQuote.length));
+    var randomNumber2 = Math.floor(Math.random() * (secondQuote.length));
+    var randomNumber3= Math.floor(Math.random() * (thirdQuote.length));
+    for (let i = 0; i < firstQuote.length + secondQuote.length + thirdQuote.length; i++)
+      var randomQuote = (" + firstQuote[randomNumber1] + "," + secondQuote[randomNumber2] + "," + thirdQuote[randomNumber3] + ");
+
+      console.log(randomQuote);
+}
