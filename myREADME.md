7c6a447c14d28df3188cd7699338cb52bfdc680a
Tanosimboangy <jacquit.aja@onja.org>
2020-06-05 07:35:49 +0300

    change some variables

---
+++ myREADME.md
@@ -0,0 +1,12 @@
+## My logic explaining how I solved the problem :
+
+-   I created a output for the user to access the data. And then, I declared all the variable by using var and an array to contain the value of each variable. I created a function and for loop to execute the variable inside of the function.  
+-   Push your solution on Github before 16h30, and send me the link to your repository on slack.
+
+## Skills to be validated
+
+-   Design an algorithm to meet specifications
+-   Implement JavaScript syntax in a program
+-   Clean code (indentation, comments, clear variables and functions names)
+
+## Good luck, and happy coding ✨
--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,11 +1,6 @@
-var firstQuote = ["The brightest light casts the darkest shadow. You will be required to do wrong no matter where you go. It is the basic condition of life, to be required to violate your own identity."];
-// The first qoute for this object. Declalration for each variable. 
-
-var secondQuote = ["To condense fact from the vapor of nuance. No matter where you go, everyone’s connected. All those moments will be lost in time, like tears in rain."];
-// The second qoute for this object. Declalration for each variable.
-
-var thirdQuote = ["Falling burned and blinded through a Siberian sky. It’s impossible to move, to live, to operate at any level without leaving traces. See, the world is full of things more powerful than us. But if you know how to catch a ride, you can go places."];
-// The third qoute for this object. Declalration for each variable.
+var firstQuote = [`Vero` , `Paul`, `Frank`, `Flore`];
+var secondQuote = [`loathe`, `hates`, `loves`, `ignores`, `abhor`];
+var thirdQuote = [`bein alone`, `the book`, `the new phone.`, `the school system`];
 
 function generateQuote() {
   var randomNumber1 = Math.floor(Math.random() * (firstQuote.length));
@@ -15,17 +10,30 @@ function generateQuote() {
     var randomQuote = firstQuote[randomNumber1] + ` ` + secondQuote[randomNumber2] + ` ` + ` ` + thirdQuote[randomNumber4];
 }
 
-for (i = 0; i < generateQuote.length; i++) {
-	if (generateQuote === firstQuote) {
-		return firstQuote;
-	} else if (generateQuote === secondQuote) {
-		return secondQuote;
-	} else {
-		return thirdQuote;
-	}
+
+
+var beg = [`Peter `, `Michael`, `Lorna`, `Diana`, `Maria`]
+var mid = [`prefer`, `looks after`, `has`, `works with`, `is breeding`]
+var end = [`dobermans`, `labradors`, `grate danes`, `french buldogs`, `rotweilers`]
+function generateQuote() {
+  var randomNumber1 = Math.floor(Math.random() * (beg.length));
+  var randomNumber2 = Math.floor(Math.random() * (mid.length));
+  var randomNumber4 = Math.floor(Math.random() * (end.length));
+  for (let i = 0; i < beg.length + mid.length + end.length; i++)
+    var randomQuote = beg[randomNumber1] + ` ` + mid[randomNumber2] + ` ` + ` ` + end[randomNumber4]
+  document.getElementById('quoteDisplay').innerHTML = [randomQuote];
 }
-
-
-var quoteNumber = prompt("How many quotes you would like to generate?", "1");
-// Create the first uotput for the user to write by using var. 
+var begg = [`She `, `He`, `It`, `Maddie`, `Josh`]
+var midd = [`likes`, `hates`, `loves`, `dislikes`, `adores`]
+var midd1 = [`well- trained`, `nice`, `adorable`, `fluffy`, `goodlooking`]
+var endd = [`animals`, `pets`, `dogs`, `cats`, `spiders`]
+function generateQuote1() {
+  var randomNumber1 = Math.floor(Math.random() * (begg.length));
+  var randomNumber2 = Math.floor(Math.random() * (midd.length));
+  var randomNumber3 = Math.floor(Math.random() * (midd1.length));
+  var randomNumber4 = Math.floor(Math.random() * (endd.length));
+  var randomQuote = begg[randomNumber1] + ` ` + midd[randomNumber2] + ` ` + midd1[randomNumber3] + ` ` + endd[randomNumber4]
+  document.getElementById('quoteDisplay').innerHTML = [randomQuote];
+}
+var quoteNumber = prompt(`How many quotes you would like to generate?`, `1`);
 if (quotenumber != null) {}
